19d55210d94eda65966dfab14f66bdeedea8579b
Nan Monnand Deng <monnand@gmail.com>
2013-10-27 16:21:29 -0400

    MessageHandler

---
+++ src/me/monnand/uniqush/demo/MessageEcho.java
@@ -0,0 +1,113 @@
+package me.monnand.uniqush.demo;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.uniqush.android.MessageHandler;
+import org.uniqush.client.Message;
+
+import android.content.Context;
+import android.util.Log;
+
+public class MessageEcho implements MessageHandler {
+
+	private String TAG = "ECHO";
+	private Context context;
+
+	public MessageEcho(Context context) {
+		Log.i(TAG, "Constructing the handler...");
+		this.context = context;
+	}
+
+	private void printMap(Map<String, String> header) {
+		Iterator<Entry<String, String>> iter = header.entrySet().iterator();
+		while (iter.hasNext()) {
+			Entry<String, String> entry = iter.next();
+			Log.i(TAG, "[" + entry.getKey() + "=" + entry.getValue() + "]");
+		}
+	}
+
+	private void printMessage(Message msg) {
+		Map<String, String> header = msg.getHeader();
+		printMap(header);
+	}
+
+	@Override
+	public void onCloseStart() {
+		Log.i(TAG, "onCloseStart");
+	}
+
+	@Override
+	public void onClosed() {
+		Log.i(TAG, "onClosed");
+
+	}
+
+	@Override
+	public void onError(Exception e) {
+		Log.i(TAG, "onErrorx " + e.toString());
+	}
+
+	@Override
+	public void onMissingAccount() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onServiceDestroyed() {
+		Log.i(TAG, "onServiceDestroyed");
+	}
+
+	@Override
+	public void onResult(int id, Exception e) {
+		String reason = "";
+		if (e != null) {
+			reason = ": " + e.getMessage();
+		}
+		Log.i(TAG, "onResult: " + id + reason);
+	}
+
+	@Override
+	public void onMessageFromServer(String dstService, String dstUser,
+			String id, Message msg) {
+		Log.i(TAG, "Message Received from server with id " + id);
+		byte[] body = { 1, 2, 3 };
+		msg.put(body);
+		/*
+		 * if (msg.get("stop") != null) { this.center.stop(context); }
+		 */
+		// this.center.sendMessageToServer(this.context, 0, msg);
+		printMessage(msg);
+
+	}
+
+	@Override
+	public void onMessageFromUser(String dstService, String dstUser,
+			String srcService, String srcUser, String id, Message msg) {
+		Log.i(TAG, "Message Received from user with id " + id + "; service="
+				+ srcService + "; username=" + srcUser + ": ");
+		printMessage(msg);
+		// this.center.sendMessageToUser(context, 1, service, username, msg,
+		// 3600);
+
+	}
+
+	@Override
+	public void onMessageDigestFromServer(String dstService, String dstUser,
+			int size, String id, Map<String, String> parameters) {
+		Log.i(TAG, "Message digest Received from server with id " + id + ": ");
+		// this.center.requestMessage(context, 30, id);
+
+	}
+
+	@Override
+	public void onMessageDigestFromUser(String dstService, String dstUser,
+			String srcService, String srcUser, int size, String id,
+			Map<String, String> parameters) {
+		Log.i(TAG, "Message digest Received from user with message id " + id
+				+ "; service=" + srcService + "; username=" + srcUser + ": ");
+		// this.center.requestMessage(context, 30, id);
+	}
+}
